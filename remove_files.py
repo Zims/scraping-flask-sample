4225e797eb42888efd51662d47c354b284583e04
zims <zims@inbox.lv>
2021-04-16 13:02:20 +0300

    except clause

--- remove_files.py
+++ remove_files.py
@@ -2,11 +2,14 @@ import os, time
 
 def cleanup():
     print("Cleanup code")
-    # path = r"output"
-    # now = time.time()
-    # arr = os.listdir(path)
-    # for f in os.listdir(path):
-    #     f = os.path.join(path, f)
-    #     if os.stat(f).st_mtime < now - 3 * 86400:
-    #         if os.path.isfile(f):
-    #             os.remove(os.path.join('', f))
+    try:
+        path = r"output"
+        now = time.time()
+        arr = os.listdir(path)
+        for f in os.listdir(path):
+            f = os.path.join(path, f)
+            if os.stat(f).st_mtime < now - 3 * 86400:
+                if os.path.isfile(f):
+                    os.remove(os.path.join('', f))
+    except:
+        pass
