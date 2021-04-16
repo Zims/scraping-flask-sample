1d539b1391574e78f8302f2d93d8984db5b91638
zims <zims@inbox.lv>
2021-04-16 13:00:09 +0300

    Cleanup code

--- remove_files.py
+++ remove_files.py
@@ -1,11 +1,12 @@
 import os, time
 
 def cleanup():
-    path = r"output"
-    now = time.time()
-    arr = os.listdir(path)
-    for f in os.listdir(path):
-        f = os.path.join(path, f)
-        if os.stat(f).st_mtime < now - 3 * 86400:
-            if os.path.isfile(f):
-                os.remove(os.path.join('', f))
+    print("Cleanup code")
+    # path = r"output"
+    # now = time.time()
+    # arr = os.listdir(path)
+    # for f in os.listdir(path):
+    #     f = os.path.join(path, f)
+    #     if os.stat(f).st_mtime < now - 3 * 86400:
+    #         if os.path.isfile(f):
+    #             os.remove(os.path.join('', f))
