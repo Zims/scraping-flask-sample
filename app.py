cdb97bd7f724639630a0157c6e232c5e64eedcd7
zims <zims@inbox.lv>
2021-04-17 09:01:37 +0300

    ss runs under the hood

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask.helpers import send_file
 import soup_yp_wo_ads
 import remove_files
 import os
+from scrapers.ss_property import scrape_ss
 
 app = Flask(__name__)
 
@@ -18,35 +19,44 @@ admin = Blueprint('admin', __name__, static_folder='static')
 def index():
     return render_template('/index.html')
 
+
 @app.route('/ss')
 def ss():
-   return render_template('ss_input.html')
+    return render_template('ss_input.html')
+
+
+@app.route('/parse_ss')
+def parse_ss():
+   scrape_ss()
+   return render_template('/ss_parsed.html')
 
 
 @app.route('/input')
 def input():
-   return render_template('input.html')
+    return render_template('input.html')
 
-@app.route('/result',methods = ['POST', 'GET'])
+
+@app.route('/result', methods=['POST', 'GET'])
 def result():
-         # return render_template("result.html",result = result)
-   if request.method == 'POST':
-      result = request.form
+    # return render_template("result.html",result = result)
+    if request.method == 'POST':
+        result = request.form
     # how to parse the flask dict:
     # https://stackoverflow.com/questions/23205577/python-flask-immutablemultidict
-      the_url = request.form.getlist('Name')
-      file_name_chosen = request.form.getlist('Filename')
-      if 'yellowpages' in request.form['Name']:
-         remove_files.cleanup()
-         soup_yp_wo_ads.output_file(the_url[0], file_name_chosen[0])
-         return render_template("result.html",result = result, json_name = soup_yp_wo_ads.json_name)
-      else:
-         return render_template('error.html')
+        the_url = request.form.getlist('Name')
+        file_name_chosen = request.form.getlist('Filename')
+        if 'yellowpages' in request.form['Name']:
+            remove_files.cleanup()
+            soup_yp_wo_ads.output_file(the_url[0], file_name_chosen[0])
+            return render_template("result.html", result=result, json_name=soup_yp_wo_ads.json_name)
+        else:
+            return render_template('error.html')
+
 
 @app.route('/output/<json_name>', methods=['POST', 'GET'])
 def otput_folder(json_name):
-   return send_from_directory('./output', f'{json_name}')
+    return send_from_directory('./output', f'{json_name}')
 
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
---
+++ scrapers/ss_property.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import time
+
+
+def scrape_ss():
+    def parse_page(page=1):
+        for row in rows:
+            d = {}
+            elements = row.find_all("td", {"class": "msga2-o pp6"})
+            element_list = []
+            for i in elements:
+                element_list.append(i.text)
+            try:
+                d["address"] = element_list[0]
+                d["istabas"] = element_list[1]
+                d["platiba"] = element_list[2]
+                d["stavs"] = element_list[3]
+                d["tips"] = element_list[4]
+                d["cena_m2"] = element_list[5].replace(" €", "")
+                d["cena(eiro)"] = element_list[-1].replace("  €", "")
+            except:
+                d["address"] = None
+                d["istabas"] = None
+                d["platiba"] = None
+                d["stavs"] = None
+                d["tips"] = None
+                d["cena_m2"] = None
+                d["cena(eiro)"] = None
+            d_list.append(d)
+
+
+
+    headers = {'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'}
+    d_list = []
+
+    for page in range(1, 5):
+        url = f"https://www.ss.com/lv/real-estate/flats/riga/centre/sell/page{page}.html"
+        response = requests.get(url, headers=headers)
+        content = response.text
+        soup = BeautifulSoup(content, "html.parser")
+        table = soup.select("table:nth-child(3)")
+        rows = table[0].find_all("tr")
+        time.sleep(2)
+        print(page)
+        parse_page(page)
+
+
+
+
+    df = pd.DataFrame(d_list)
+    df.dropna().to_csv("output.csv")
+    df.dropna().to_excel("output.xlsx")
+    print("Done!")
