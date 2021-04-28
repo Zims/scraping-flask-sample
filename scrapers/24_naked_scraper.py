92447ea11f48b91fb7aaae3ba4a4c67eb428b2c0
zims <zims@inbox.lv>
2021-04-28 15:06:29 +0300

    single page scraper

--- scrapers/24_naked_scraper.py
+++ scrapers/24_naked_scraper.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 import time
 
-
+d_list = []
 headers = {'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'}
 
 
@@ -19,48 +19,29 @@ table = soup.find("div", {"id": "list-container"})
 list_item = table.find_all("li", {"class": "new result regular"})
 print(len(list_item))
 
-for i in list_item:
-    address = i.find("a", {"class": "addressLink"}).find("span").text.split(",")[0]
-    price = int(i.find("div", {"class": "price"}).find("div").text.replace(" EUR", "").replace(" ", "").strip())
-    istabas = int(i.find("div", {"class": "column"}).find("ol").find_all("li")[1].find("strong").text)
-    platiba = float(i.find("div", {"class": "column"}).find("ol").find_all("li")[0].find("strong").text.split(" ")[0])
-    stavs = i.find("div", {"class": "column"}).find("ol").find_all("li")[2].find("strong").text
-    if i.find_all("div", {"class": "column"})[1].find("div", {"class": "promo"}).find("span") == None:
-        promo = "Nav"
-    else:
-        promo = i.find_all("div", {"class": "column"})[1].find("div", {"class": "promo"}).find("span").text
-
-        
-    print(f'***{promo}')
-
-# print(table)
-# rows = table[0].find_all("tr")
-
-# for row in rows:
-#     d = {}
-#     elements = row.find_all("td", {"class": "msga2-o pp6"})
-#     element_list = []
-#     for i in elements:
-#         element_list.append(i.text)
-#     try:
-#         d["address"] = element_list[0]
-#         try:
-#             d["istabas"] = int(element_list[1])
-#         except:
-#             d["istabas"] = "Nav norādīts"
-
-#         d["platiba"] = int(element_list[2])
-#         d["stavs"] = element_list[3]
-#         d["tips"] = element_list[4]
-#         d["cena_m2"] = int(element_list[5].replace(" €", "").replace(",", ""))
-#         d["cena(eiro)"] = int(element_list[-1].replace("  €", "").replace(",", ""))
-#         d["Vieta"] = chosen_region[0]
-#     except:
-#         d["address"] = None
-#         d["istabas"] = None
-#         d["platiba"] = None
-#         d["stavs"] = None
-#         d["tips"] = None
-#         d["cena_m2"] = None
-#         d["cena(eiro)"] = None
-#     d_list.append(d)
+def parse_page_city24():
+    for i in list_item:
+        d = {}
+        try:
+            d["address"] = i.find("a", {"class": "addressLink"}).find("span").text.split(",")[0]
+            d["istabas"] = int(i.find("div", {"class": "column"}).find("ol").find_all("li")[1].find("strong").text)
+            d["platiba"] = float(i.find("div", {"class": "column"}).find("ol").find_all("li")[0].find("strong").text.split(" ")[0])
+            d["stavs"] = i.find("div", {"class": "column"}).find("ol").find_all("li")[2].find("strong").text
+            d["promo"] = i.find_all("div", {"class": "column"})[1].find("div", {"class": "promo"}).find("span").text
+            
+            d["price_m2"] = float(i.find("div", {"class": "price_sqrm"}).text.replace(" ", "").replace("EUR/m²", "").replace(",", "."))
+            d["price"] = int(i.find("div", {"class": "price"}).find("div").text.replace(" EUR", "").replace(" ", "").strip())
+            
+            # No good TODO. Use scraper input
+            d["vieta"] = i.find("a", {"class": "addressLink"}).find("span").text.split(",")[1]
+        except:
+            d["address"] = None
+            d["istabas"] = None
+            d["platiba"] = None
+            d["stavs"] = None
+            d["promo"] = None
+            d["price_m2"] = None
+            d["price"] = None
+            d["vieta"] = None
+        d_list.append(d)
+    print(d_list[5])
